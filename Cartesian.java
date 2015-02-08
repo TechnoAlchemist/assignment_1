8727642058b2e8074657b4bcdefad9ddf83f5119
Gabe Lafontant <glafonta@gmail.com>
2015-02-08 16:57:33 -0500

    beginning of cartesian program

---
+++ Cartesian.java
@@ -0,0 +1,25 @@
+/* Gabe Lafontant glafonta@gmail.com
+*/
+
+import java.util.Scanner;
+
+public class Cartesian {
+  public static void  main(String[] args) {
+
+    int x, y, z;
+
+    Scanner scan = new Scanner(Sytem.in);
+
+    System.out.println("This is a Cartesian World. Please enter some coordinates.");
+
+    System.out.print("Enter an X-coordinate");
+    x = scan.nextInt();
+
+    System.out.print("Enter an Y-coordinate");
+    y = scan.nextInt();
+
+    System.out.print("Enter an Z-coordinate");
+    z = scan.nextInt();
+
+  }
+}
